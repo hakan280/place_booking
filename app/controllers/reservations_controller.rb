532e9e02151240d2d308523389e6b4c7bc11ce7b
hakan280 <hkara419@gmail.com>
2016-08-17 10:32:55 +0300

    Authenticate reservation create action for customer

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,10 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_customer! , only: [:create]
   def create
-      @reservation = Reservation.new(reservation_params)
-      if @reservation.save
-        redirect_to place_path(id: params[:place_id]), notice: "Your reservation succesfully"
-      end
+        @reservation = Reservation.new(reservation_params)
+        if @reservation.save
+          redirect_to place_path(id: params[:place_id]), notice: "Your reservation succesfully"
+        end
   end
   def show
   end
